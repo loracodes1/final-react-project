f5335a7b3e7ab22e99d067b01e43ed5909ad1721
florence <loracodes3@gmail.com>
2024-11-12 15:59:34 +0300

    created json server,the recipelist component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import RecipeDetails from './RecipeDetails';
 
 function App() {
   return (
-    
+
   );
 }
 
--- src/Components/RecipeList.js
+++ src/Components/RecipeList.js
@@ -1,17 +1,36 @@
 // src/components/RecipeList.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const RecipeList = ({ recipes }) => {
+const RecipeList = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/recipes")
+      .then((response) => response.json())
+      .then((data) => setRecipes(data));
+  }, []);
+
+  const filteredRecipes = recipes.filter(recipe =>
+    recipe.title.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
     <div>
       <h1>Recipe List</h1>
+      <input
+        type="text"
+        placeholder="Search recipes..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       <ul>
-        {recipes.map((recipe) => (
+        {filteredRecipes.map((recipe) => (
           <li key={recipe.id}>
             <Link to={`/recipe/${recipe.id}`}>
               <h3>{recipe.title}</h3>
-              <p>{recipe.description}</p>
+              <p>{recipe.description?.substring(0, 100)}...</p>
             </Link>
           </li>
         ))}
