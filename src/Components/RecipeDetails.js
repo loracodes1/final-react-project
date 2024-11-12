4bc4a7bb8c57e6ccbbeedb2cd6e9f90b6a8454c3
florence <loracodes3@gmail.com>
2024-11-12 16:02:19 +0300

    adding recipe details

---
+++ src/Components/RecipeDetails.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const RecipeDetails = () => {
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/recipes/${id}`)
+      .then((response) => response.json())
+      .then((data) => setRecipe(data));
+  }, [id]);
+
+  return recipe ? (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p><strong>Ingredients:</strong> {recipe.ingredients}</p>
+      <p><strong>Instructions:</strong> {recipe.instructions}</p>
+      <p><strong>Time:</strong> {recipe.time}</p>
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
+};
+
+export default RecipeDetails;
--- src/Components/RecipeList.js
+++ src/Components/RecipeList.js
@@ -1,4 +1,3 @@
-// src/components/RecipeList.js
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
