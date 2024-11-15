af416fa0c86eff9e46d17c99702bce33b3ab5937
florence <loracodes3@gmail.com>
2024-11-15 05:10:45 +0300

    deploy backend to render

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 const config = {
-    base_url: "http://localhost:6001",
+    // base_url: "http://localhost:6001",
+    base_url: "https://final-react-project-43ph.onrender.com",
     food_db_base_url: "https://www.themealdb.com",
 };
 
